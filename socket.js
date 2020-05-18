8d696dde902c2af2fa0633c0f924e2acdf7ad699
YJ <yunjeong.syj@gmail.com>
2020-05-18 16:41:17 +0900

    change web socket to Socket.IO

--- socket.js
+++ socket.js
@@ -1,28 +1,25 @@
-const WebSocket = require('ws');
+const SocketIO = require('socket.io');
 
 module.exports = (server) => {
-	const wss = new WebSocket.Server({ server });
+	const io = SocketIO(server, {path: '/socket.io' });
 
-	wss.on('connection', (ws, req) => {
+	io.on('connection', (socket) => {
+		const req = socket.request;
 		const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-		console.log(' Access New Client', ip);
-		ws.on('message', (message) => {
-			console.log(message);
+		console.log('Access New Client', ip, socket.id, req.ip);
+		socket.on('disconnect', () => {
+			console.log('Client Access Finished', ip, socket.id);
+			clearInterval(socket.interval);
 		});
-		ws.on('error', (error) => {
+		socket.on('error', (error) => {
 			console.error(error);
 		});
-		ws.on('close', () => {
-			console.log('Client Access Finish', ip);
-			clearInterval(ws.interval);
+		socket.on('reply', (data) => {
+			console.log(data);
 		});
 
-		const interval = setInterval(()=>{
-			if(ws.readyState === ws.OPEN) {
-				ws.send('Send Message Server To Client');
-			}
+		socket.interval  = setInterval(()=>{
+			socket.emit('news', 'Hello Socket.IO');
 		}, 3000);
-		ws.interval = interval;
-
 	});
 };
